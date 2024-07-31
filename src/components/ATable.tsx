1f4055ecf64463c21e34266c792adf599714c6c1
colton <1679963578@qq.com>
2024-07-31 14:50:23 +0800

    update

--- src/components/ATable.tsx
+++ src/components/ATable.tsx
@@ -9,17 +9,17 @@ type ATableProps = {
 const ATable: FC<ATableProps> = ({ header = [], data = [] }) => {
     return <>
 
-        <table className=" overflow-auto">
+        <table className=" overflow-x-auto">
             <tr className=" bg-[#FAFAFA] rounded-lg h-[50px] border-b-[#FFFFFF] ">
                 {header.map((item, i) => {
-                    return <th key={`table_${i}`} className=" w-[200px]">{item.name}</th>
+                    return <th key={`table_${i}`} className=" w-[400px]">{item.name}</th>
                 })}
             </tr>
 
             {data.length > 0 && data.map((item, i) => {
                 return <tr className="bg-[#FFFFFF] border-b" key={`col_${i}`}>
                     <td>{item.fileName}</td>
-                    <td>{truncateMiddle(item.id, 6, 6)}</td>
+                    <td>{truncateMiddle(item.id, 5, 5)}</td>
                     <td>{formatBytes(Number(item.fileSize))}</td>
                     <td>{timestampToDateTime(Number(item.uploadDate))}</td>
                     <td>{item.from}</td>
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,9 +6,9 @@ export const timestampToDateTime = (timestamp: number) => {
     const day = ('0' + date.getDate()).slice(-2);
     const hours = ('0' + date.getHours()).slice(-2);
     const minutes = ('0' + date.getMinutes()).slice(-2);
-    const seconds = ('0' + date.getSeconds()).slice(-2);
+    // const seconds = ('0' + date.getSeconds()).slice(-2);
 
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
 };
 
 export const formatBytes = (bytes: number) => {
