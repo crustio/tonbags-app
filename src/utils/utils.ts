8289f01797b4536b6ad0cffc6c5d6efc852762a7
colton <1679963578@qq.com>
2024-10-23 15:40:42 +0800

    fix

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,7 @@
 import { toast } from 'sonner';
 
 export const timestampToDateTime = (timestamp: number) => {
-    const date = new Date((timestamp * 1000) / 1000);
+    const date = new Date(timestamp * 1000);
 
     const year = date.getFullYear();
     const month = ('0' + (date.getMonth() + 1)).slice(-2);
