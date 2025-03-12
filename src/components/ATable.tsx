eb3d3dc1e60077d8358bcafc115bc7ce2f861fb0
Colton Kentner <31772839+YongZL@users.noreply.github.com>
2025-03-12 13:25:19 +0800

    update ui
    update ui

--- src/components/ATable.tsx
+++ src/components/ATable.tsx
@@ -64,11 +64,19 @@ const ATable = React.forwardRef(({ header = [], data = [], loading, className }:
             </tr>
             {!loading && data.length > 0 && data.map((item, i) => {
                 return <tr className="bg-[#FFFFFF] border-b  odd:bg-slate-50  pl-3" key={`col_${i}`}>
+                    <td><div className="w-[80px] flex gap-2 ">
+                        <button onClick={() => copyTextToClipboard(onShare(item))}>
+                            <img src="share.svg" className="w-5" />
+                        </button>
+                        <button onClick={() => onDownloadFile(item)}>
+                            <img src="download.svg" className="w-5" />
+                        </button>
+                    </div></td>
                     <td><div id="not-clickable"
                         data-tooltip-id="my-tooltip"
                         data-tooltip-content={item.fileName}
                         data-tooltip-delay-hide={100}
-                        data-tooltip-place='top-start' className="w-[200px]  pl-2  h-[40px] flex items-center">{truncateMiddle(item.fileName, 6, 6)}</div></td>
+                        data-tooltip-place='top-start' className="w-[160px]  pl-2  h-[40px] flex items-center">{truncateMiddle(item.fileName, 6, 6)}</div></td>
                     <td>
                         <div className="w-[130px]  flex items-center justify-between mr-5  ">{truncateMiddle(item.bagId || item.cid, 5, 5)}
                             <button onClick={() => copyTextToClipboard(item.bagId || item.cid)}>
@@ -85,14 +93,7 @@ const ATable = React.forwardRef(({ header = [], data = [], loading, className }:
                     >{formatBytes(Number(item.fileSize))}</div></td>
                     <td><div className="w-[150px]">{timestampToDateTime(Number(item.uploadDate))}</div></td>
                     <td><div className="w-[100px]">{truncateMiddle(item.from, 5, 5)}</div></td>
-                    <td><div className="w-[100px] flex gap-2">
-                        <button onClick={() => copyTextToClipboard(onShare(item))}>
-                            <img src="share.svg" className="w-5" />
-                        </button>
-                        <button onClick={() => onDownloadFile(item)}>
-                            <img src="download.svg" className="w-5" />
-                        </button>
-                    </div></td>
+
                 </tr>
 
             })}
